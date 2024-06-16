e2aa3bf7d2c837bdedb9743c92539a109af545f5
Zain Zafar <zainzafar90@gmail.com>
2024-06-17 01:03:30 +0500

    chore: dashboard project configuration and files and configure medusa

---
+++ apps/dashboard/src/app/app.tsx
@@ -0,0 +1,13 @@
+import { Alert } from '@medusajs/ui';
+import { Button, Drawer } from '@medusajs/ui';
+
+export function App() {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Alert variant="success">Here's a message</Alert>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
