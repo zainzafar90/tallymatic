37ae853b8f8069d80152690828c4abaa88eb588c
Zain Zafar <zainzafar90@gmail.com>
2024-08-14 16:52:49 +0500

    nit

--- apps/dashboard-v2/src/app/app.tsx
+++ apps/dashboard-v2/src/app/app.tsx
@@ -2,7 +2,7 @@ import { Toaster } from '@medusajs/ui';
 import { QueryClientProvider } from '@tanstack/react-query';
 
 import { queryClient } from '@/lib/query-client';
-import { RouterProvider } from '@/providers/router-provider';
+import { RouterProvider } from '@/providers/router';
 import { ThemeProvider } from '@/providers/theme/theme-provider';
 
 export function App() {
