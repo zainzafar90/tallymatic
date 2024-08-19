61714af6fe20ea77a28573cc5cccc600b926c876
Zain Zafar <zainzafar90@gmail.com>
2024-08-19 06:31:38 +0500

    refactor: Update user model email validation message

--- apps/api/src/database/seeders/20240818185303-generate-users.js
+++ apps/api/src/database/seeders/20240818185303-generate-users.js
@@ -7,9 +7,21 @@ module.exports = {
       'users',
       [
         {
-          id: 'd3a3b4b6-4b7b-4b7b-9b4b-7b4b7b4b7b5f',
+          id: 'd3a3b4b6-4b7b-4b7b-9b4b-7b4b7b4b7b1f',
           organizationId: 'd3a3b4b6-4b7b-4b7b-9b4b-7b4b7b4b7b4b',
-          name: 'Admin Tallymatic',
+          name: 'Super Admin',
+          email: 'superadmin@tallymatic.com',
+          contact: '...',
+          password: '$2a$08$tHoJggaL5aFhM/wEGpVlb.CUPuj5NqdO4EEtE.x37s.FA9U77haYG', // test1234
+          isEmailVerified: true,
+          role: 'super-admin',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 'd3a3b4b6-4b7b-4b7b-9b4b-7b4b7b4b7b2f',
+          organizationId: 'd3a3b4b6-4b7b-4b7b-9b4b-7b4b7b4b7b4b',
+          name: 'Admin',
           email: 'admin@tallymatic.com',
           contact: '...',
           password: '$2a$08$tHoJggaL5aFhM/wEGpVlb.CUPuj5NqdO4EEtE.x37s.FA9U77haYG', // test1234
@@ -19,9 +31,9 @@ module.exports = {
           updatedAt: new Date(),
         },
         {
-          id: 'd3a3b4b6-4b7b-4b7b-9b4b-7b4b7b4b7c5f',
+          id: 'd3a3b4b6-4b7b-4b7b-9b4b-7b4b7b4b7b3f',
           organizationId: 'd3a3b4b6-4b7b-4b7b-9b4b-7b4b7b4b7b4b',
-          name: 'User Tallymatic',
+          name: 'User',
           email: 'user@tallymatic.com',
           contact: '...',
           password: '$2a$08$tHoJggaL5aFhM/wEGpVlb.CUPuj5NqdO4EEtE.x37s.FA9U77haYG', // test1234
